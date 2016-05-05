2ddafd3b030c7d6763b6009c1f0fa37d0db7f7e1
User <User@Desktop>
2016-05-05 22:34:46 +0200

    a

--- SurvivalGame/src/survivalgame/SurvivalGame.java
+++ SurvivalGame/src/survivalgame/SurvivalGame.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package survivalgame;
 
-/**
- *
- * @author Abdelrahman Ossama
- */
 public class SurvivalGame {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        main_menu mm = new main_menu();
+        mm.setVisible(true);
+    }    
 }
