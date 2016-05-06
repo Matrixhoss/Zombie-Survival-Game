21059caf7dd8985906c0a75734f10998b79e7014
Hesham <Hesham@Hesham-Pc>
2016-05-06 15:30:29 +0300

    Merge origin/master
    Conflicts:
    	src/teamstack/Accounts.java

--- SurvivalGame/src/survivalgame/Character.java
+++ SurvivalGame/src/survivalgame/Character.java
@@ -4,14 +4,18 @@ public abstract class Character {
     
     private int speed;
     private int health;
-    private String weapon;
+    private Weapons weapon;
     
-    
+    public Character(int s ,int h, Weapons W){
+    this.speed=s;
+    this.health=h;
+    this.weapon=W;
+    }
     
     public void move(){};
     public void die(){};
     public void takeDamage(){};
-    
+    public void setWeapon(){};
     
     
 }
