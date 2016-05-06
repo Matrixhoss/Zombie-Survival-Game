6aca7784a8c3d50c34a34d118f2b988ccb328180
Hesham <>
2016-05-06 13:46:36 +0200

    Hesham shit

--- SurvivalGame/src/survivalgame/Character.java
+++ SurvivalGame/src/survivalgame/Character.java
@@ -1,10 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package survivalgame;
 
-public class Character {
+public abstract class Character {
+    
+    private int speed;
     private int health;
+    private String weapon;
+    
+    
+    
+    public void move(){};
+    public void die(){};
+    public void takeDamage(){};
+    
+    
+    
 }
