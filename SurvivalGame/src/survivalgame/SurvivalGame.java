432ea9e314179954f00b65ca53f8730e6dc5f7bb
Hesham <Hesham@Hesham-Pc>
2016-05-06 15:10:09 +0300

    Merge origin/master
    Conflicts:
    	src/teamstack/Accounts.java

--- SurvivalGame/src/survivalgame/SurvivalGame.java
+++ SurvivalGame/src/survivalgame/SurvivalGame.java
@@ -4,7 +4,5 @@ public class SurvivalGame {
     public static void main(String[] args) {
         main_menu mm = new main_menu();
         mm.setVisible(true);
-        Map m = new Map();
-        m.setVisible(true);
     }    
 }
