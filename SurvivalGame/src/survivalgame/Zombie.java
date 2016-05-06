12c8e4f3f5161c0ae56bb0270a528e4d563704f2
Hesham <Hesham@Hesham-Pc>
2016-05-06 15:53:40 +0300

    Merge origin/master
    Conflicts:
    	src/teamstack/Accounts.java

--- SurvivalGame/src/survivalgame/Zombie.java
+++ SurvivalGame/src/survivalgame/Zombie.java
@@ -1,10 +1,42 @@
 
 package survivalgame;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Zombie extends Character {
+    private String Type;
     
     
+    public Zombie (int health,int speed,Weapons Weapon,String Type){
+        super(health,speed,Weapon);
+        this.Type=Type;
+        
+    }
+    
+    public void move(){
+    
+    
+    
+    }
+    @Override
+    public void die(){
+      if (this.getHealth()==0)
+         try {
+             this.finalize();
+      } catch (Throwable ex) {
+          Logger.getLogger(Zombie.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    
+    }
+    public void takeDamage(int d ){
+    
+    
+    }
+    public void setWeapon(){
+    
+    }
     
     
 }
