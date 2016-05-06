8d71518948162e92238678d36fd5e011fa6af7b0
Hesham <Hesham@Hesham-Pc>
2016-05-06 18:00:25 +0300

    SUKA

---
+++ SurvivalGame/src/survivalgame/Position.java
@@ -0,0 +1,24 @@
+
+package survivalgame;
+
+
+
+
+public class Position {
+    
+    private int x;
+    private int y;
+    
+    Position(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    
+    
+}
