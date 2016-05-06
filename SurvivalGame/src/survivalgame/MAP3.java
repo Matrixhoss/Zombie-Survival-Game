d546637c084edb212b106182083a158a02fa6f39
User <User@Desktop>
2016-05-06 17:46:45 +0200

    asd

---
+++ SurvivalGame/src/survivalgame/MAP3.java
@@ -0,0 +1,35 @@
+
+package survivalgame;
+
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MAP3 extends JFrame {
+    private Zombie z ;
+    private ImagePanel Background;
+    
+    ImageIcon MapIcn= new ImageIcon(getClass().getResource("misc/wd.png"));
+    ImageIcon Zombierawr= new ImageIcon(getClass().getResource("misc/Sprites/ZombieWalk_normal_scaled_fast.gif"));
+    
+    public MAP3(){
+        
+    Container c = this.getContentPane();
+    
+    Background = new ImagePanel(MapIcn.getImage());
+    c.add(Background);
+     
+    this.pack();
+    this.setVisible(true);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setResizable(false);
+    
+    z= new Zombie(100,200,"asd");
+    z.setIcon(Zombierawr);
+    z.setBounds(50,50, 200, 400);
+    Background.add(z);
+    
+    }
+ }
