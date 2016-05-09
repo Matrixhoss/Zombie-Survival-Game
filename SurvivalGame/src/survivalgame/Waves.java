0dd6c210a3836a031d563bb8412ec25cdbf6f87b
Hesham <Hesham@Hesham-Pc>
2016-05-09 18:23:28 +0300

    Zombies spawn from corners

--- SurvivalGame/src/survivalgame/Waves.java
+++ SurvivalGame/src/survivalgame/Waves.java
@@ -3,13 +3,13 @@ package survivalgame;
 
 
 public  class Waves {
-    private static int WaveNumber;
+    private static int WaveNumber=1;
     
     public static void setNextWave(){
     Waves.WaveNumber+=1;
     }
-    public int getWave(){
-    return Waves.WaveNumber;
+    public static int getWave(){
+    return WaveNumber;
     }
  
 }
