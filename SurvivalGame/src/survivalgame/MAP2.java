72fba61ded4a729c095ca0d03837c594fe60ee2a
AhmedQuickTech <AhmedQuickTech@Ahmed>
2016-05-09 19:23:19 +0200

    asd

--- SurvivalGame/src/survivalgame/MAP2.java
+++ SurvivalGame/src/survivalgame/MAP2.java
@@ -3,33 +3,125 @@ package survivalgame;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.ImageObserver;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class MAP2 extends JFrame {
-    private Zombie z ;
+    private ZombieNormal z ;
     private ImagePanel Background;
-    
+    private ZombieGenerator zn;
+    private ZombieGenerator zf;
+    private Player p ;
     ImageIcon MapIcn= new ImageIcon(getClass().getResource("misc/Hillsides.png"));
     ImageIcon Zombierawr= new ImageIcon(getClass().getResource("misc/Sprites/ZombieWalk_normal_scaled_fast.gif"));
+    ImageIcon Playericon  = new ImageIcon(getClass().getResource("misc/Sprites/Soldier.png"));
+    private Random r;
+    private Random R;
+    private Dimension mapdim = new Dimension();
     
+    
+    private int TotalNumberOfZombies;
     public MAP2(){
-        
+ 
+  
     Container c = this.getContentPane();
     
     Background = new ImagePanel(MapIcn.getImage());
     c.add(Background);
+    mapdim=Background.getSize();
+       
+        
      
+   
+   
+       r= new Random();
+    
+
+      zn=new ZombieGenerator("ZombieNormal");
+        for (int i = 0; i < zn.getZombieNumber(); i++) {
+              int x = r.nextInt(1400);
+              int y= r.nextInt(800);
+              int loc=r.nextInt(5);
+              
+                  switch (loc){
+                      case 1 : zn.z.get(i).setBounds(x, 780, 100,100);
+                              break;
+                      case 2:zn.z.get(i).setBounds(0, y, 100,100);
+                              break;
+                      case 3 :zn.z.get(i).setBounds(x, 0, 100,100);
+                              break;
+                      case 4: zn.z.get(i).setBounds(1200, y, 100,100);
+                              break;
+                  }
+            Background.add(zn.z.get(i));
+        }  
+//    
+        
+ 
+   
+    p = new Player (100,10,this,20,20,Background,mapdim,zn);
+       p.setIcon(Playericon);
+       p.setSize(100, 100);
+       Background.add(p);
+        
+   
+    Timer t=new Timer(30,new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < zn.z.size(); i++){
+            zn.z.get(i).AI(p.getX(),p.getY());
+//            zf.z.get(i).AI(p.getX(),p.getY());
+        }
+        if (zn.z.isEmpty()){
+            Waves.setNextWave();
+            zn.updateZombie("ZombieNormal");
+            GenerateNormal();
+        }
+           //z.move();
+           p.move(mapdim);
+           p.FireHandling();
+           p.setLocation(p.getx(), p.gety());
+           System.out.println(Waves.getWave());
+        }
+        
+        
+   
+   
+    });
+    t.start();
     this.pack();
     this.setVisible(true);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setResizable(false);
-//    
-//    z= new Zombie(100,200,"asd");
-//    z.setIcon(Zombierawr);
-//    z.setBounds(50,50, 200, 400);
-//    Background.add(z);
-    
     }
+       public void GenerateNormal(){
+//       zn=new ZombieGenerator("ZombieNormal");
+        for (int i = 0; i < zn.getZombieNumber(); i++) {
+              int x = r.nextInt(1400);
+              int y= r.nextInt(800);
+              int loc=r.nextInt(5);
+              
+                  switch (loc){
+                      case 1 : zn.z.get(i).setBounds(x, 780, 100,100);
+                              break;
+                      case 2:zn.z.get(i).setBounds(0, y, 100,100);
+                              break;
+                      case 3 :zn.z.get(i).setBounds(x, 0, 100,100);
+                              break;
+                      case 4: zn.z.get(i).setBounds(1200, y, 100,100);
+                              break;
+                  }
+            Background.add(zn.z.get(i));
+        }}
  }
