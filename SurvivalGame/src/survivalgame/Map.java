ff2f9a4254d690f7e1b9bfa9d127c2dedb7d0ddd
Hassan Tarek <hassantarek1997@gmail.com>
2016-05-13 15:16:12 +0200

    P2

--- SurvivalGame/src/survivalgame/Map.java
+++ SurvivalGame/src/survivalgame/Map.java
@@ -146,7 +146,9 @@ public class Map extends JFrame {
                     p.die();
            
                 }
-                if(NumberOfPlayers==2){
+                
+            }
+            if(NumberOfPlayers==2){
                if((Math.abs(zn.z.get(i).getX()-p2.getX())<=60)&&(Math.abs(zn.z.get(i).getY()-p2.getY())<=60)){
                 p2.takeDamage(zn.z.get(i).weapon.damage);
                 System.out.println(p2.getHealth());
@@ -157,7 +159,6 @@ public class Map extends JFrame {
                     p2.setEnabled(false);
                     p2.setVisible(false);
                     p2.die();}}}
-            }
             }
             }
             setHealthBar();
